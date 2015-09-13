aac4bddc34c7e388ebfc9fb08d015f26a073ea56
JanL <janelle.h.writes@gmail.com>
2015-09-13 13:48:17 -0400

    Checkpoint 9 completed, strings

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,35 +12,51 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    return [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+//    return nil;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
+    
+    NSString *returnString = cheeseName;
+    
     if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
         /* WORK HERE, ASSUMING `cheeseName` ENDS WITH " cheese" */
-    } else {
-        /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
+    NSRange cheeseRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch | NSBackwardsSearch];
+        returnString = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
     }
+        
+//        
+//    } else {
+//        
+//        /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
+//    
+//    }
 
     /*
      (You will learn more about if/else statements in a later checkpoint.)
      */
 
-    return nil;
+    return returnString;
+//    return nil;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
-    if (cheeseCount == 1) {
-        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
-    } else {
-        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
-    }
-    
-    /*
-     (You will learn more about if/else statements in a later checkpoint.)
-     */
-    
-    return nil;
+    NSString *cheeseString = (cheeseCount == 1 ? @"cheese" : @"cheeses");
+    return [NSString stringWithFormat:@"%lu %@", (unsigned long)cheeseCount, cheeseString];
 }
+    
+//    if (cheeseCount == 1) {
+//        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+//    } else {
+//        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+//    }
+//    
+//    /*
+//     (You will learn more about if/else statements in a later checkpoint.)
+//     */
+//    
+//    return nil;
+//}
 
 @end
